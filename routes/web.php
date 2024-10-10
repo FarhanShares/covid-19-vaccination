ea54b1853f49cf35a4e3d50b15a756ad18f242f8
Farhan Israq <farhan.israq.bd@gmail.com>
2024-10-10 23:03:24 +0600

    feat: added home route name

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Livewire\Register;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::view('/', 'welcome')->name('home');
 
 Route::middleware('guest')->group(function () {
     Route::get('/register', Register::class)->name('register');
