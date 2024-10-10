fa40f8c85e8004c2bd7a0a43b4ebb1e3d15aacf7
Farhan Israq <farhan.israq.bd@gmail.com>
2024-10-10 23:14:33 +0600

    refactor: remove unnecessary packages and codes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,6 +23,7 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        // Todo update user factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
@@ -37,7 +38,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
