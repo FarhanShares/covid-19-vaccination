0742814d37e43894c4402bb2cb9faddb1ad01f9d
Farhan Israq <farhan.israq.bd@gmail.com>
2024-10-10 21:28:01 +0600

    feat: added after clause

--- database/migrations/2024_10_10_042423_add_vaccine_center_id_column_to_users_table.php
+++ database/migrations/2024_10_10_042423_add_vaccine_center_id_column_to_users_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignIdFor(VaccineCenter::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(VaccineCenter::class)
+                ->after('status')
+                ->constrained()
+                ->cascadeOnDelete();
         });
     }
 
