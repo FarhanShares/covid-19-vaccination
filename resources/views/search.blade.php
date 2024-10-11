7fee76f6f9a596ba6e51dccbf7121a797889f806
Farhan Israq <farhan.israq.bd@gmail.com>
2024-10-11 08:07:38 +0600

    feat: search index page

---
+++ resources/views/search.blade.php
@@ -0,0 +1,10 @@
+<x-layouts.app>
+    <x-section class="max-w-xl mx-auto">
+        <h2 class="text-xl font-semibold text-black dark:text-white">Search</h2>
+        <p class="mt-0 text-sm/relaxed">
+            Look-up your registration or vaccination status by your NID.
+        </p>
+
+        <x-form-search class="block w-full" />
+    </x-section>
+</x-layouts.app>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\SearchController;
 use App\Livewire\Register;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
+
+Route::get('/search', [SearchController::class, 'index'])->name('search');
 
 Route::middleware('guest')->group(function () {
     Route::get('/register', Register::class)->name('register');
