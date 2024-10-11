745b7b568f822c4b5f21ad35924cc303a01821eb
Farhan Israq <farhan.israq.bd@gmail.com>
2024-10-11 15:09:57 +0600

    feat: registration successful route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RegistrationCompletedController;
 use App\Http\Controllers\SearchController;
 use App\Livewire\Register;
 use Illuminate\Support\Facades\Route;
@@ -9,6 +10,6 @@ Route::view('/', 'welcome')->name('home');
 Route::get('/search', [SearchController::class, 'index'])->name('search');
 Route::post('/search', [SearchController::class, 'search'])->name('search.result');
 
-Route::middleware('guest')->group(function () {
-    Route::get('/register', Register::class)->name('register');
-});
+
+Route::get('/register', Register::class)->name('register')->middleware('guest');
+Route::get('/success', RegistrationCompletedController::class)->name('success');
