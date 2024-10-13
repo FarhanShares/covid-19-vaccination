00ed7f4d34ef046772981c28956d60cdbb58907b
Farhan Israq <farhan.israq.bd@gmail.com>
2024-10-13 09:48:47 +0600

    feat: handle edge cases, early return and updated doc blocks

--- app/Jobs/BatchScheduleVaccineAppointmentJob.php
+++ app/Jobs/BatchScheduleVaccineAppointmentJob.php
@@ -30,40 +30,68 @@ class BatchScheduleVaccineAppointmentJob implements ShouldQueue
 
     /**
      * Execute the job.
+     *
+     * Instead of creating or updating the records one by one, which is slow and not performant,
+     * we'll leverage batch updates. Therefore, we'll prepare the data first and then persist it.
      */
     public function handle(): void
     {
         $users = $this->userRepository->unappointed($this->batchSize);
 
-        foreach ($users as $user) {
-            // Find the closest available date for the chosen vaccine center
-            // Booking service along with user repository takes care of the first come, first served principle
-            $appointmentDate = $this->bookingService->findDate($user->vaccineCenter);
+        if ($users->count() < 1) {
+            return;
+        }
 
-            // Schedule the appointment
-            $appointment = VaccineAppointment::create([
+        $appointments = collect([]);
+
+        foreach ($users as $user) {
+            /**
+             * Find the closest available date for the chosen vaccine center and store in in-memory array.
+             * Booking service along with user repository takes care of the "first come, first served" principle.
+             */
+            $appointmentDate = $this->bookingService->findDate(
+                vaccineCenter: $user->vaccineCenter
+            );
+            $appointments->push([
                 'date'              => $appointmentDate,
                 'user_id'           => $user->id,
                 'vaccine_center_id' => $user->vaccine_center_id,
                 'status'            => AppointmentStatus::SCHEDULED,
+                'updated_at'        => now(),
+                'created_at'        => now(),
             ]);
 
-            // Update the user user status
-            $this->userRepository->updateStatus(
-                user: $user,
-                status: VaccinationStatus::SCHEDULED,
-            );
-
-            // Notify the booking service that we have used the date
-            // which it just curated for us.
+            /**
+             * Notify the booking service that we have used the date which it just curated for us.
+             * This is also super performant because it uses temporary in-memory redis storage.
+             */
             $this->bookingService->useDate(
-                date: $appointment->date,
+                date: $appointmentDate,
                 vaccineCenterId: $user->vaccine_center_id,
             );
         }
 
-        // After processing all users in the batch, flush to persist all necessary data
-        // from temporary storage to permanent storage and clear the temporary data too.
+        // Handle an edge-case.
+        if ($appointments->count() < 1) {
+            $this->bookingService->flush(persist: false);
+            return;
+        }
+
+        // Create appointment records in batch
+        VaccineAppointment::insert(
+            values: $appointments->toArray()
+        );
+
+        // Update users vaccination status in batch
+        $this->userRepository->updateManyStatus(
+            ids: $appointments->pluck('user_id')->toArray(),
+            status: VaccinationStatus::SCHEDULED,
+        );
+
+        /**
+         * After processing all users in the batch, flush to persist all necessary data
+         * from temporary storage to permanent storage and clear the temporary data too.
+         */
         $this->bookingService->flush();
     }
 }
