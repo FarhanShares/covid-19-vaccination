fc23c2e73410d405b06997052f018a71f2eca396
Farhan Israq <farhan.israq.bd@gmail.com>
2024-10-14 15:05:59 +0600

    feat: show progress and improve output

--- app/Console/Commands/Init.php
+++ app/Console/Commands/Init.php
@@ -28,21 +28,42 @@ class Init extends Command
      */
     public function handle()
     {
-        $this->info('Clearing cache...');
-        Artisan::call('optimize:clear');
+        $this->info('Starting initialization process...');
+        $this->line('');
 
-        $this->info('Starting with a fresh migration...');
+        $this->info('1. Clearing cache...');
+        Artisan::call('optimize:clear');
+        $this->info(Artisan::output());
+        $this->line('');
+
+        $this->info('2. Starting with a fresh migration...');
         Artisan::call('migrate:fresh --force');
         $this->info(Artisan::output());
 
-        $this->info('Seeding 20 vaccine centers with random data...');
+        $this->info('3. Seeding 20 vaccine centers with random data...');
         VaccineCenter::factory()->count(20)->create();
+        $this->line('');
 
-        $this->info('Seeding 5000 users with random data...');
-        User::factory()->count(5000)->create();
+        $this->info('4. Seeding 5000 users with random data...');
+        // User::factory()->count(5000)->create();
 
-        $this->info('Optimizing...');
+        $totalUsers = 5000;
+        $batchSize  = 500;
+        $batches    = ceil($totalUsers / $batchSize);
+
+        for ($i = 0; $i < $batches; $i++) {
+            User::factory()->count(min($batchSize, $totalUsers - ($i * $batchSize)))->create();
+
+            // Output progress
+            $this->info('   > Created ' . (($i + 1) * $batchSize) . ' users out of ' . $totalUsers);
+        }
+
+        $this->line('');
+
+        $this->info('5. Optimizing the app...');
         Artisan::call('optimize');
+        $this->info(Artisan::output());
+        $this->line('');
 
         $this->alert('Initialization completed! - Farhan Israq');
     }
